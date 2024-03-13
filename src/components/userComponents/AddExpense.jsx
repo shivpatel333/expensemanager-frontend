59110c4948d97e7111fbac78d2d366f74aafb883
shivpatel333 <shivpatel605@gmail.com>
2024-03-13 14:32:07 +0530

    Chart and Filter

--- src/components/userComponents/AddExpense.jsx
+++ src/components/userComponents/AddExpense.jsx
@@ -4,14 +4,14 @@ import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
 
 export const AddExpense = () => {
-  
+
   useEffect(() => {
     const id = sessionStorage.getItem("id");
-    
+
     getUserById(id);
     getPayee();
     getCategory();
-    getSubCategory();
+    // getSubCategory();
     getTransactionType();
     getPaymentType();
     getGoalsByUserId(id);
@@ -53,16 +53,16 @@ export const AddExpense = () => {
     }
   }
 
-  const [subCategory, setsubCategory] = useState([]);
-  const getSubCategory = async () => {
-    try {
-      const res = await axios.get("http://localhost:4000/subCategory/getSubCategory");
-      setsubCategory(res.data.data);
+  // const [subCategory, setsubCategory] = useState([]);
+  // const getSubCategory = async () => {
+  //   try {
+  //     const res = await axios.get("http://localhost:4000/subCategory/getSubCategory");
+  //     setsubCategory(res.data.data);
 
-    } catch (err) {
-      console.log('getSubCategory error :', err);
-    }
-  }
+  //   } catch (err) {
+  //     console.log('getSubCategory error :', err);
+  //   }
+  // }
 
   const [transactionType, settransactionType] = useState([]);
   const getTransactionType = async () => {
@@ -88,14 +88,14 @@ export const AddExpense = () => {
   const [goals, setgoals] = useState([]);
   const getGoalsByUserId = async (usId) => {
     try {
-        const goalData = await axios.get("http://localhost:4000/goal/getGoalByUserId/" + usId);
-        //console.log("user goals...", goalData);
-        setgoals(goalData.data.data);
+      const goalData = await axios.get("http://localhost:4000/goal/getGoalByUserId/" + usId);
+      //console.log("user goals...", goalData);
+      setgoals(goalData.data.data);
 
     } catch (err) {
-        console.log("getGoalsByUserId error :", err);
+      console.log("getGoalsByUserId error :", err);
     }
-}
+  }
 
   const sendMail = async (data) => {
     try {
@@ -136,6 +136,8 @@ export const AddExpense = () => {
   const { register, handleSubmit } = useForm();
   const submitHandler = async (data) => {
     //console.log('form data', data);
+    // let dd = Object.assign(data, {user:"1234"});
+
     try {
       const res = await axios.post("http://localhost:4000/transaction/createTransaction", data);
       if (res.status === 201) {
@@ -163,7 +165,7 @@ export const AddExpense = () => {
             <div className="card-body">
               <form onSubmit={handleSubmit(submitHandler)}>
                 <div className="row">
-                  <div className="col-md-5 pr-1">
+                  <div className="col-md-4 pr-1">
                     <div className="form-group">
                       <label htmlFor='transactionDateTime'>Date & Time</label>
                       <input
@@ -171,6 +173,12 @@ export const AddExpense = () => {
                         className="form-control"
                         disabled=""
                       />
+                    </div>
+                  </div>
+                  <div className="col-md-4 pr-1">
+                    <div className="form-group">
+                      <label htmlFor='title'>Title:</label>
+                      <input type='text' {...register('title')} className="form-control"></input>
                     </div>
                   </div>
                   <div className="col-md-3 px-1">
@@ -183,16 +191,16 @@ export const AddExpense = () => {
                       </select>
                     </div>
                   </div>
-                  <div className="col-md-4 pl-1">
+                  {/* <div className="col-md-4 pl-1">
                     <div className="form-group">
                       <label htmlFor='subCategory'>Sub category:</label>
                       <select {...register('subCategory')} className="form-control">
                         {subCategory.map((dt) => {
-                          return (<option value={dt._id}>{dt.SubCategoryName}</option>)
+                          return (<option value={dt._id}>{dt.subCategoryName}</option>)
                         })}
                       </select>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
                 <div className="row">
                   <div className="col-md-6 pr-1">
@@ -246,7 +254,7 @@ export const AddExpense = () => {
                 <div className="row">
                   <div className="col-md-12">
                     <div className="form-group">
-                      <label htmlFor='description'>Goal:</label>
+                      <label htmlFor='goal'>Goal:</label>
                       <select {...register('goal')} className="form-control">
                         {goals.map((dt) => {
                           return (<option value={dt._id}>{dt.goalName}</option>)
@@ -287,5 +295,78 @@ export const AddExpense = () => {
         </div>
       </div>
     </div>
+
+    // <div>
+    //   <h1>AddExpense</h1>
+
+    //   <form onSubmit={handleSubmit(submitHandler)}>
+    //     <div>
+    //       <label htmlFor='user'>User:</label>
+    //       <select {...register('user')}>
+    //         <option value={user._id}>{user.firstName}</option>
+    //       </select>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='payee'>Payee:</label>
+    //       <select {...register('payee')}>
+    //         {payee.map((dt) => {
+    //           return (<option value={dt._id}>{dt.payeeName}</option>)
+    //         })}
+    //       </select>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='transactionDateTime'>Date-Time:</label>
+    //       <input type='datetime-local' {...register('transactionDateTime')}></input>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='category'>Category:</label>
+    //       <select {...register('category')}>
+    //         {category.map((dt) => {
+    //           return (<option value={dt._id}>{dt.categoryName}</option>)
+    //         })}
+    //       </select>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='subCategory'>Sub category:</label>
+    //       <select {...register('subCategory')}>
+    //         {subCategory.map((dt) => {
+    //           return (<option value={dt._id}>{dt.subCategoryName}</option>)
+    //         })}
+    //       </select>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='transactionType'>Transaction type:</label>
+    //       <select {...register('transactionType')}>
+    //         {transactionType.map((dt) => {
+    //           return (<option value={dt._id}>{dt.transactionType}</option>)
+    //         })}
+    //       </select>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='paymentType'>Payment type:</label>
+    //       <select {...register('paymentType')}>
+    //         {paymentType.map((dt) => {
+    //           return (<option value={dt._id}>{dt.paymentType}</option>)
+    //         })}
+    //       </select>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='amount'>Amount:</label>
+    //       <input type='text' {...register('amount')}></input>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='description'>Description:</label>
+    //       <textarea {...register('description')}></textarea>
+    //     </div>
+    //     <div>
+    //       <label htmlFor='status'>Status:</label>
+    //       <input type='text' {...register('status')}></input>
+    //     </div>
+    //     <div>
+    //       <input type='submit' value='submit'></input>
+    //     </div>
+    //   </form>
+
+    // </div>
   )
 }
